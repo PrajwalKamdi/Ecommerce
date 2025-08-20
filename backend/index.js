429bdab311a4ce6c2edd9d2115de95190d01b108
PrajwalKamdi <prajwalkamdi18@gmail.com>
2025-08-20 11:58:45 +0530

    Small Issued Fixed

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,7 @@ server.use('/api/cart', cart_router)
 server.listen(PORT, () => {
   console.log("Server running on port ", PORT)
 })
-
+server.get('/', (req, res) => {
+  res.send('API is running...') 
+})
 export default server;
